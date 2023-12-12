fd66d9a0b84fdc34f2d5a346eb8179ba3302db82
MATHUR-LUV <luvm1403@gmail.com>
2023-12-13 01:02:53 +0530

    files upload

---
+++ js/client.js
@@ -0,0 +1,40 @@
+const socket = io("http://localhost:8000");
+const form = document.getElementById('send_container');
+const messageinput = document.getElementById('message_input');
+const messagecontainer = document.querySelector(".container");
+var audio = new Audio('button-124476.mp3');
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messagecontainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+
+    }
+}
+const name = prompt("Enter your name to join the chat ");
+socket.emit('new-user-joined',name);
+
+
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat`,'right');
+
+})
+
+socket.on('receive', data=>{
+    append(`${data.name} : ${data.message}`,'left');
+})
+
+socket.on('left',name=>{
+    append(`${name} left the chat`,'right');
+})
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageinput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageinput.value = ''
+})
